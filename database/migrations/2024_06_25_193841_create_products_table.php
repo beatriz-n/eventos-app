f09f364e50d471ba2b660d829b02b989a1223973
beatriz-n <beatriznespolid@gmail.com>
2024-06-25 16:56:44 -0300

    configuração

--- database/migrations/2024_06_25_193841_create_products_table.php
+++ database/migrations/2024_06_25_193841_create_products_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 100);
+            $table->integer('qty');
+            $table->text('description');
             $table->timestamps();
         });
     }
