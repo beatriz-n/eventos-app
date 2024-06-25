8570170f3da5fc8b119b68e14e06d90aceb93592
beatriz-n <beatriznespolid@gmail.com>
2024-06-25 16:59:27 -0300

    configurações

--- database/migrations/2024_06_25_195435_add_category_to_products_table.php
+++ database/migrations/2024_06_25_195435_add_category_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('category', 100);
+            $table->dropColumn('category');
         });
     }
 
