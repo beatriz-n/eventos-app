6b60493d7310929f90a57e7591adaf98f185e86a
beatriz-n <beatriznespolid@gmail.com>
2024-06-28 14:21:05 -0300

    aula 17

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,34 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 use App\Models\Event;
 
 class EventController extends Controller
 {
-    public function index()
-    {
+    
+    public function index() {
+
         $events = Event::all();
-        return view('welcome', [
-            'events' => $events
-        ]);
+    
+        return view('welcome',['events' => $events]);
+
     }
 
-    public function create()
-    {
+    public function create() {
         return view('events.create');
     }
 
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
+
         $event = new Event;
+
         $event->title = $request->title;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+
+        }
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
+
     }
+
 }
